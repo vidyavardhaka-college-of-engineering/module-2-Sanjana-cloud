4c4a906e90971ebe0975e144d5dd6bd4f4c0ba84
Sanjana-cloud <84697388+Sanjana-cloud@users.noreply.github.com>
2021-06-13 13:35:02 +0530

    program 4 done

--- Program-4.c
+++ Program-4.c
@@ -6,6 +6,7 @@
 int main ()
 {
  int a=10,sum = 0,i,n;
+ scanf("%d",&n);
  
 //Fill the missing code
  printf("Enter n value");
@@ -16,10 +17,9 @@ for(i=1;i<=n;i++)
    
    if ( i % 2 == 0 )
       continue;
-  sum = sum + ;
+  sum = sum + i;
   }
 //Fill the missing code
 printf("%d",sum);
  return 0;
 }
-
